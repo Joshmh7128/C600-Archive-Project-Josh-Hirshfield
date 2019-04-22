392bdf9576fa31a7da6283e1624dbeacc245b164
Josh <joshmh7128@gmail.com>
2019-04-21 20:28:19 -0400

    update

---
+++ Assets/Scripts/CheckScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckScript : MonoBehaviour
+{
+    public string station;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(station + " activated");
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log(station + " deactivated");
+    }
+}
