10c41de0780c2e069faf3cffd458dd4dfc7ad393
Josh <joshmh7128@gmail.com>
2019-04-21 22:10:58 -0400

    Added sample media
    Video currently does not work

--- Assets/Scripts/CheckScript.cs
+++ Assets/Scripts/CheckScript.cs
@@ -5,14 +5,31 @@ using UnityEngine;
 public class CheckScript : MonoBehaviour
 {
     public string station;
+    public GameObject SampleAudio;
+    bool isPlaying;
+    public GameObject CurrentAudio;
 
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log(station + " activated");
+
+        if (station == "Audio")
+        {
+            if (!isPlaying)
+            {
+                CurrentAudio = Instantiate(SampleAudio);
+                isPlaying = true;
+            }
+        }
     }
 
     private void OnCollisionExit(Collision collision)
     {
         Debug.Log(station + " deactivated");
+        if (station == "Audio")
+        {
+            CurrentAudio.GetComponent<Destroy>().DestroyObject();
+            isPlaying = false;
+        }
     }
 }
